6c073c0b0d3d586a1c9419bd9af99175b7f3082c
Matt Wilson <matthew.wilson.synesis@gmail.com>
2023-12-31 14:13:02 +1100

    ~ license

--- include/systemtools/clasp.hpp
+++ include/systemtools/clasp.hpp
@@ -4,11 +4,11 @@
  * Purpose:     C++ layer for the CLASP library.
  *
  * Created:     31st January 2014
- * Updated:     18th April 2019
+ * Updated:     31st December 2023
  *
  * Home:        https://github.com/synesissoftware/CLASP/
  *
- * Copyright (c) 2014-2019, Matthew Wilson
+ * Copyright (c) 2014-2023, Matthew Wilson
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -16,18 +16,18 @@
  * met:
  *
  * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
+ *   this list of conditions and the following disclaimer;
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- * - Neither the names of Matthew Wilson and Synesis Software nor the names
- *   of any contributors may be used to endorse or promote products derived
- *   from this software without specific prior written permission.
+ *   documentation and/or other materials provided with the distribution;
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -54,8 +54,8 @@
 #ifndef SYSTEMTOOLS_DOCUMENTATION_SKIP_SECTION
 # define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_MAJOR    1
 # define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_MINOR    0
-# define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_REVISION 1
-# define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_EDIT     3
+# define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_REVISION 2
+# define SYSTEMTOOLS_VER_SYSTEMTOOLS_HPP_CLASP_EDIT     4
 #endif /* !SYSTEMTOOLS_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
--- include/systemtools/clasp/util/search_specifications.hpp
+++ include/systemtools/clasp/util/search_specifications.hpp
@@ -16,18 +16,18 @@
  * met:
  *
  * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
+ *   this list of conditions and the following disclaimer;
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- * - Neither the names of Matthew Wilson and Synesis Software nor the names
- *   of any contributors may be used to endorse or promote products derived
- *   from this software without specific prior written permission.
+ *   documentation and/or other materials provided with the distribution;
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -54,8 +54,8 @@
 #ifndef SYSTEMTOOLS_DOCUMENTATION_SKIP_SECTION
 # define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_MAJOR     1
 # define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_MINOR     6
-# define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_REVISION  4
-# define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_EDIT      18
+# define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_REVISION  5
+# define SYSTEMTOOLS_VER_SYSTEMTOOLS_CLASP_UTIL_HPP_SEARCH_SPECIFICATIONS_EDIT      19
 #endif /* !SYSTEMTOOLS_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
