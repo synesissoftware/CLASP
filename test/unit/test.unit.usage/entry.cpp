bf6f6dcd842e722f7b980afbcea837194ab8e286
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-10 19:12:37 +1100

    **test.unit.usage** : + added some tests for existing functionality

--- test/unit/test.unit.usage/entry.cpp
+++ test/unit/test.unit.usage/entry.cpp
@@ -24,7 +24,7 @@
  */
 
 /* xTests header files */
-#include <xtests/xtests.h>
+#include <xtests/terse-api.h>
 #include <xtests/util/temp_file.hpp>
 
 /* STLSoft header files */
@@ -58,6 +58,9 @@ namespace
     static void TEST_showBody_one_option_one_flag();
     static void TEST_showBody_single_flag_changing_positiveTabSizes();
     static void TEST_showBody_single_flag_changing_consoleWidths();
+
+    static void TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_ZERO_TABS_AND_NO_BLANK_LINES();
+    static void TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_neg2_TABS_AND_NO_BLANK_LINES();
 } // anonymous namespace
 
 
@@ -84,6 +87,9 @@ int main(int argc, char **argv)
         XTESTS_RUN_CASE(TEST_showBody_one_option_one_flag);
         XTESTS_RUN_CASE(TEST_showBody_single_flag_changing_positiveTabSizes);
         XTESTS_RUN_CASE(TEST_showBody_single_flag_changing_consoleWidths);
+
+        XTESTS_RUN_CASE(TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_ZERO_TABS_AND_NO_BLANK_LINES);
+        XTESTS_RUN_CASE(TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_neg2_TABS_AND_NO_BLANK_LINES);
 
         XTESTS_PRINT_RESULTS();
 
@@ -134,6 +140,168 @@ namespace
             ,   consoleWidth
             ,   showBlanksBetweenItems
             );
+
+            scoper.close();
+
+            typedef platformstl::basic_file_lines<
+                char
+            ,   string_t
+            >                                               file_lines_t;
+
+            file_lines_t    file_lines(temp.c_str());
+            strings_t       lines;
+
+            lines.reserve(file_lines.size());
+
+            for (file_lines_t::const_iterator i = file_lines.begin(); file_lines.end() != i; ++i)
+            {
+                lines.push_back(*i);
+            }
+
+            return lines;
+        }
+    }
+
+    static
+    strings_t
+    get_showUsage_lines(
+        clasp_specification_t const specifications[]
+    ,   clasp_char_t const*         toolName
+    ,   clasp_char_t const*         summary
+    ,   clasp_char_t const*         copyright
+    ,   clasp_char_t const*         description
+    ,   clasp_char_t const*         usage
+    ,   int                         major
+    ,   int                         minor
+    ,   int                         revision
+    ,   int                         flags
+    ,   int                         consoleWidth
+    ,   int                         tabSize
+    ,   int                         blanksBetweenItems
+    )
+    {
+        using namespace ::xtests::cpp::util;
+
+        temp_file   temp(temp_file::DeleteOnOpen | temp_file::DeleteOnClose);
+        FILE* const stm = ::fopen(temp.c_str(), "w");
+
+        if (NULL == stm)
+        {
+            throw std::runtime_error("could not open file");
+        }
+        else
+        {
+            stlsoft::scoped_handle<FILE*> scoper(stm, ::fclose);
+
+            clasp_showUsage(
+                NULL
+            ,   specifications
+            ,   toolName
+            ,   summary
+            ,   copyright
+            ,   description
+            ,   usage
+            ,   major, minor, revision
+            ,   clasp_showHeaderByFILE
+            ,   clasp_showBodyByFILE
+            ,   stm
+            ,   flags
+            ,   consoleWidth
+            ,   tabSize
+            ,   blanksBetweenItems
+            );
+
+
+            scoper.close();
+
+            typedef platformstl::basic_file_lines<
+                char
+            ,   string_t
+            >                                               file_lines_t;
+
+            file_lines_t    file_lines(temp.c_str());
+            strings_t       lines;
+
+            lines.reserve(file_lines.size());
+
+            for (file_lines_t::const_iterator i = file_lines.begin(); file_lines.end() != i; ++i)
+            {
+                lines.push_back(*i);
+            }
+
+            return lines;
+        }
+    }
+
+    static
+    strings_t
+    get_show_usage_lines(
+        clasp_specification_t const specifications[]
+    ,   clasp_char_t const*         toolName
+    ,   clasp_char_t const*         summary
+    ,   clasp_char_t const*         copyright
+    ,   clasp_char_t const*         description
+    ,   clasp_char_t const*         usage
+    ,   int                         major
+    ,   int                         minor
+    ,   int                         revision
+    ,   int                         flags
+    ,   int                         consoleWidth
+    ,   int                         tabSize
+    ,   int                         blanksBetweenItems
+    )
+    {
+        using namespace ::xtests::cpp::util;
+
+        temp_file   temp(temp_file::DeleteOnOpen | temp_file::DeleteOnClose);
+        FILE* const stm = ::fopen(temp.c_str(), "w");
+
+        if (NULL == stm)
+        {
+            throw std::runtime_error("could not open file");
+        }
+        else
+        {
+            stlsoft::scoped_handle<FILE*> scoper(stm, ::fclose);
+
+            {
+#if 0
+#elif defined(__GNUC__)
+
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#else
+#endif
+
+                {
+                    clasp_show_usage(
+                        NULL
+                    ,   specifications
+                    ,   toolName
+                    ,   summary
+                    ,   copyright
+                    ,   description
+                    ,   usage
+                    ,   major, minor, revision
+                    ,   clasp_show_header_by_FILE
+                    ,   clasp_show_body_by_FILE
+                    ,   stm
+                    ,   flags
+                    ,   consoleWidth
+                    ,   tabSize
+                    ,   blanksBetweenItems
+                    );
+                }
+
+#if 0
+#elif defined(__GNUC__)
+
+# pragma GCC diagnostic pop
+#else
+#endif
+                }
+
+
 
             scoper.close();
 
@@ -435,6 +603,188 @@ static void TEST_showBody_single_flag_changing_consoleWidths()
         XTESTS_TEST_MULTIBYTE_STRING_EQUAL(" --flag1", lines[2]);
         XTESTS_TEST_MULTIBYTE_STRING_EQUAL("  this is a", lines[3]);
         XTESTS_TEST_MULTIBYTE_STRING_EQUAL("  flag", lines[4]);
+    }
+}
+
+static void TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_ZERO_TABS_AND_NO_BLANK_LINES()
+{
+    clasp_specification_t const specifications[] =
+    {
+        CLASP_FLAG(NULL, "--flag1", "this is a flag"),
+        CLASP_OPTION(NULL, "--option1", "this is an option", ""),
+
+        CLASP_SPECIFICATION_ARRAY_TERMINATOR
+    };
+    clasp_char_t const* const   toolName            =   "myapp";
+    clasp_char_t const* const   summary             =   "My tools";
+    clasp_char_t const* const   copyright           =   "Copyright (c) Me! 2025";
+    clasp_char_t const* const   description         =   "This app does things";
+    clasp_char_t const* const   usage               =   "USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]";
+    int const                   major               =   1;
+    int const                   minor               =   2;
+    int const                   revision            =   3;
+    int const                   flags               =   0;
+    int const                   consoleWidth        =   2000;
+    int const                   tabSize             =   0;
+    int const                   blanksBetweenItems  =   0;
+
+    // `clasp_showUsage()`
+    {
+        strings_t lines = get_showUsage_lines(
+            specifications
+        ,   toolName
+        ,   summary
+        ,   copyright
+        ,   description
+        ,   usage
+        ,   major
+        ,   minor
+        ,   revision
+        ,   flags
+        ,   consoleWidth
+        ,   tabSize
+        ,   blanksBetweenItems
+        );
+
+        REQUIRE(TEST_INTEGER_EQUAL(13u, lines.size()));
+        TEST_MS_EQ("My tools", lines[0]);
+        TEST_MS_EQ("myapp version 1.2.3.0", lines[1]);
+        TEST_MS_EQ("Copyright (c) Me! 2025", lines[2]);
+        TEST_MS_EQ("This app does things", lines[3]);
+        TEST_MS_EQ("", lines[4]);
+        TEST_MS_EQ("USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]", lines[5]);
+        TEST_MS_EQ("", lines[6]);
+        TEST_MS_EQ("Flags and options:", lines[7]);
+        TEST_MS_EQ("", lines[8]);
+        TEST_MS_EQ("--flag1", lines[9]);
+        TEST_MS_EQ("this is a flag", lines[10]);
+        TEST_MS_EQ("--option1=<value>", lines[11]);
+        TEST_MS_EQ("this is an option", lines[12]);
+    }
+
+    // `get_show_usage_lines()`
+    {
+        strings_t lines = get_show_usage_lines(
+            specifications
+        ,   toolName
+        ,   summary
+        ,   copyright
+        ,   description
+        ,   usage
+        ,   major
+        ,   minor
+        ,   revision
+        ,   flags
+        ,   consoleWidth
+        ,   tabSize
+        ,   blanksBetweenItems
+        );
+
+        REQUIRE(TEST_INTEGER_EQUAL(13u, lines.size()));
+        TEST_MS_EQ("My tools", lines[0]);
+        TEST_MS_EQ("myapp version 1.2.3.0", lines[1]);
+        TEST_MS_EQ("Copyright (c) Me! 2025", lines[2]);
+        TEST_MS_EQ("This app does things", lines[3]);
+        TEST_MS_EQ("", lines[4]);
+        TEST_MS_EQ("USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]", lines[5]);
+        TEST_MS_EQ("", lines[6]);
+        TEST_MS_EQ("Flags and options:", lines[7]);
+        TEST_MS_EQ("", lines[8]);
+        TEST_MS_EQ("--flag1", lines[9]);
+        TEST_MS_EQ("this is a flag", lines[10]);
+        TEST_MS_EQ("--option1=<value>", lines[11]);
+        TEST_MS_EQ("this is an option", lines[12]);
+    }
+}
+
+static void TEST_clasp_showUsage_AND_clasp_show_usage_NORMAL_1_neg2_TABS_AND_NO_BLANK_LINES()
+{
+    clasp_specification_t const specifications[] =
+    {
+        CLASP_FLAG(NULL, "--flag1", "this is a flag"),
+        CLASP_OPTION(NULL, "--option1", "this is an option", ""),
+
+        CLASP_SPECIFICATION_ARRAY_TERMINATOR
+    };
+    clasp_char_t const* const   toolName            =   "myapp";
+    clasp_char_t const* const   summary             =   "My tools";
+    clasp_char_t const* const   copyright           =   "Copyright (c) Me! 2025";
+    clasp_char_t const* const   description         =   "This app does things";
+    clasp_char_t const* const   usage               =   "USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]";
+    int const                   major               =   1;
+    int const                   minor               =   2;
+    int const                   revision            =   3;
+    int const                   flags               =   0;
+    int const                   consoleWidth        =   2000;
+    int const                   tabSize             =   -2;
+    int const                   blanksBetweenItems  =   0;
+
+    // `clasp_showUsage()`
+    {
+        strings_t lines = get_showUsage_lines(
+            specifications
+        ,   toolName
+        ,   summary
+        ,   copyright
+        ,   description
+        ,   usage
+        ,   major
+        ,   minor
+        ,   revision
+        ,   flags
+        ,   consoleWidth
+        ,   tabSize
+        ,   blanksBetweenItems
+        );
+
+        REQUIRE(TEST_INTEGER_EQUAL(13u, lines.size()));
+        TEST_MS_EQ("My tools", lines[0]);
+        TEST_MS_EQ("myapp version 1.2.3.0", lines[1]);
+        TEST_MS_EQ("Copyright (c) Me! 2025", lines[2]);
+        TEST_MS_EQ("This app does things", lines[3]);
+        TEST_MS_EQ("", lines[4]);
+        TEST_MS_EQ("USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]", lines[5]);
+        TEST_MS_EQ("", lines[6]);
+        TEST_MS_EQ("Flags and options:", lines[7]);
+        TEST_MS_EQ("", lines[8]);
+        TEST_MS_EQ("  --flag1", lines[9]);
+        TEST_MS_EQ("    this is a flag", lines[10]);
+        TEST_MS_EQ("  --option1=<value>", lines[11]);
+        TEST_MS_EQ("    this is an option", lines[12]);
+    }
+
+    // `get_show_usage_lines()`
+    {
+        strings_t lines = get_show_usage_lines(
+            specifications
+        ,   toolName
+        ,   summary
+        ,   copyright
+        ,   description
+        ,   usage
+        ,   major
+        ,   minor
+        ,   revision
+        ,   flags
+        ,   consoleWidth
+        ,   tabSize
+        ,   blanksBetweenItems
+        );
+
+        REQUIRE(TEST_INTEGER_EQUAL(13u, lines.size()));
+        TEST_MS_EQ("My tools", lines[0]);
+        TEST_MS_EQ("myapp version 1.2.3.0", lines[1]);
+        TEST_MS_EQ("Copyright (c) Me! 2025", lines[2]);
+        TEST_MS_EQ("This app does things", lines[3]);
+        TEST_MS_EQ("", lines[4]);
+        TEST_MS_EQ("USAGE: myapp [ ... flags / options ...] <value-1> [ ... <value-N> ]", lines[5]);
+        TEST_MS_EQ("", lines[6]);
+        TEST_MS_EQ("Flags and options:", lines[7]);
+        TEST_MS_EQ("", lines[8]);
+        TEST_MS_EQ("  --flag1", lines[9]);
+        TEST_MS_EQ("    this is a flag", lines[10]);
+        TEST_MS_EQ("  --option1=<value>", lines[11]);
+        TEST_MS_EQ("    this is an option", lines[12]);
     }
 }
 } // anonymous namespace
