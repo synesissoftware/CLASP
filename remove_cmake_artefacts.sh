01bb51160fa81d3ec6ccd892597a40cc3d38ec61
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-07-03 14:11:52 +1000

    chore: helper scripts now implemented in terms of **/.sis/script_info_lines.txt**

--- remove_cmake_artefacts.sh
+++ remove_cmake_artefacts.sh
@@ -56,10 +56,8 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     --help)
 
+      [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
       cat << EOF
-CLASP is a small, simple C-language library for parsing command-line arguments, along with a C++ header-only API.
-Copyright (c) 2019-2025, Matthew Wilson and Synesis Information Systems
-Copyright (c) 2008-2019, Matthew Wilson and Synesis Software
 Removes all known CMake artefacts
 
 $ScriptPath [ ... flags/options ... ]
--- run_all_unit_tests.sh
+++ run_all_unit_tests.sh
@@ -32,10 +32,8 @@ while [[ $# -gt 0 ]]; do
       ;;
     --help)
 
+      [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
       cat << EOF
-CLASP is a small, simple C-language library for parsing command-line arguments, along with a C++ header-only API.
-Copyright (c) 2019-2025, Matthew Wilson and Synesis Information Systems
-Copyright (c) 2008-2019, Matthew Wilson and Synesis Software
 Runs all (matching) component and unit test programs
 
 $ScriptPath [ ... flags/options ... ]
